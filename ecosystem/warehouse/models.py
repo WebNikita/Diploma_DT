b690e2e3ebbe65f7addb2b236dae2b946d6d8e20
Proot <proot@Proot-desktop.Proot>
2022-01-07 16:04:05 +0300

    marge

--- ecosystem/warehouse/models.py
+++ ecosystem/warehouse/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
+from accounts.models import Teacher
 # Create your models here.
 
 class Extradition(models.Model):
