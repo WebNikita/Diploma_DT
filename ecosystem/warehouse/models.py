fda5af8d9155001dcc298b32c8ead8f30591b5ab
Proot <proot@Proot-desktop.Proot>
2022-01-07 15:23:58 +0300

    models

--- ecosystem/warehouse/models.py
+++ ecosystem/warehouse/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Extradition(models.Model):
+    name = models.CharField(max_length=128)
+    type = models.CharField(max_length=128)
+    cell = models.CharField(max_length=128)
+    quantity = models.IntegerField()
+    data = models.DateTimeField()
+    user = models.CharField(max_length=128)
+
+class Shipment(models.Model):
+    name = models.CharField(max_length=128)
+    type = models.CharField(max_length=128)
+    cell = models.CharField(max_length=128)
+    quantity = models.IntegerField()
+    data = models.DateField()
+    # user = models.CharField(max_length=128)
+class Warehouse(models.Model):
+    name = models.CharField(max_length=128)
+    type = models.CharField(max_length=128)
+    cell = models.CharField(max_length=128)
+    quantity = models.IntegerField()
+    remains = models.IntegerField()
+    data = models.DateTimeField()
