2e9cc29b3fbbf67ff110f9a094b77ac813b36592
Prom1x <kirill.zhu46@gmail.com>
2022-02-03 12:58:50 +0300

    search

--- ecosystem/warehouse/urls.py
+++ ecosystem/warehouse/urls.py
@@ -3,12 +3,12 @@ from django.contrib.auth import views
 from django.urls import path
 
 # from ecosystem.warehouse.models import Category
-from .views import Warehouse, Catalog, Search
+from .views import Warehouse, Catalog, SearchResultsView
 
 app_name = 'warehouse'
 
 urlpatterns = [
     path('', Warehouse.as_view(), name='warehouse'),
+    path('search/',SearchResultsView.as_view(), name='search_product'),
     path('<str:slug>/', Catalog.as_view(), name='render_catalog'),
-    path('search/',Search.as_view(), name='search_product')
 ]
--- ecosystem/warehouse/views.py
+++ ecosystem/warehouse/views.py
@@ -1,6 +1,8 @@
 
 # from pyexpat import model
+from itertools import product
 from pyexpat import model
+from unicodedata import name
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
@@ -51,15 +53,29 @@ class Catalog(TemplateView):
         return context
 
 
-class Search(ListView):
+class SearchResultsView(ListView):
     model = Warehouse
     template_name = 'warehouse/index.html'
 
     def get_queryset(self):
         query = self.request.GET.get('search')
-        print(self.request.GET)
-        object_list = Warehouse.objects.filter(
-            Q(name__icontains=query) | Q(cell__icontains=query)
-            )
+        print(query)
+        # products = Category.objects.all().product.all()
+        # print(products)
+        object_list = Warehouse.objects.filter(Q(name__icontains=query))
         print(object_list)
         return object_list
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        category_list = Category.objects.all()
+        # products = Category.objects.get(slug = self.kwargs['slug']).products.all()
+        # print(products)
+
+        context['catalog_list'] = category_list
+        # context['products'] = products
+        
+        print(context)
+
+        return context
