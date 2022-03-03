a7ea67d25323b072dacfbb8a3d1d179c44468380
Prom1x <kirill.zhu46@gmail.com>
2022-03-03 13:26:33 +0300

    None

--- ecosystem/warehouse/views.py
+++ ecosystem/warehouse/views.py
@@ -37,7 +37,15 @@ class Home(TemplateView):
         print(context)
         return context
 
-
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated == True:
+            if request.method.lower() in self.http_method_names:
+                handler = getattr(self, request.method.lower(), self.http_method_not_allowed)
+            else:
+                handler = self.http_method_not_allowed
+            return handler(request, *args, **kwargs)
+        else:
+            return redirect("/accounts/login/")
 
 class Catalog(TemplateView):
     
